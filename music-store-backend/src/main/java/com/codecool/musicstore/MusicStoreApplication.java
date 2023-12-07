907fd6b90eaffa01a3bacb762adc079711ec0447
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2023-12-07 11:10:05 +0100

    Add guitar model and refactor application

--- music-store-backend/src/main/java/com/codecool/musicstore/MusicStoreApplication.java
+++ music-store-backend/src/main/java/com/codecool/musicstore/MusicStoreApplication.java
@@ -7,19 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusicStoreApplication {
-private ProductDao productDao;
-@Autowired
-	public MusicStoreApplication(ProductDao productDao) {
-		this.productDao = productDao;
-	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(MusicStoreApplication.class, args);
 	}
 
-	@PostConstruct
-	public void seedDatabase() {
-		productDao.populateDataBase();
-	}
 
 }
