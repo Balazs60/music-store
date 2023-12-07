8b6d7a8ad09abda3cfde9c5639c73c7ae03c42ed
Borisz Bisits <118134911+BoriszBisits@users.noreply.github.com>
2023-12-07 11:53:01 +0100

    create structure and add seed to guitars

---
+++ music-store-backend/src/main/java/com/codecool/musicstore/Dao/GuitarDao.java
@@ -0,0 +1,17 @@
+package com.codecool.musicstore.Dao;
+
+import com.codecool.musicstore.model.Guitar;
+import com.codecool.musicstore.repositories.GuitarRepositoriy;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface GuitarDao {
+
+    public void saveGuitar(Guitar guitar);
+    public Guitar getGuitarById(UUID id);
+    public List<Guitar> getAllGuitar();
+    public void  saveGuitars(List<Guitar> guitars);
+
+    public  void seedGuitars();
+}
