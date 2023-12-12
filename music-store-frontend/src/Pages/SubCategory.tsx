4a6335e67ee9a7937d6c4c85541c323ee1bb3f20
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2023-12-12 20:53:05 +0100

    Add filter by price to products with slider and refactor pictures path in product generator

--- music-store-frontend/src/Pages/SubCategory.tsx
+++ music-store-frontend/src/Pages/SubCategory.tsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
+import Slider from 'react-slider';
+import '../musicStore.css'
 
 interface Product {
     id: string;
@@ -16,13 +18,17 @@ interface Product {
 
 }
 
+
 function SubCategory() {
     const [products, setProducts] = useState<Product[]>([]);
     const [filteredProducts, setFilteredProducts] = useState<Product[]>([]);
-    const [minPrice, setMinPrice] = useState<string | ''>('');
-    const [maxPrice, setMaxPrice] = useState<string | ''>('');
+    const [minPrice, setMinPrice] = useState<number | ''>(0);
+    const [maxPrice, setMaxPrice] = useState<number | ''>(0);
+    const [lowestPrice, setLowestPrice] = useState<number>(0);
+    const [highestPrice, setHighestPrice] = useState<number>(0);
     const [selectedBrands, setSelectedBrands] = useState<string[]>([])
     const { category, subcategoryid } = useParams();
+    const [values, setValues] = useState([lowestPrice,highestPrice])
     const navigate = useNavigate();
 
     console.log("cat " + category);
@@ -31,6 +37,10 @@ function SubCategory() {
     useEffect(() => {
         fetchProducts();
     }, []);
+
+    useEffect(() => {
+        setValues([lowestPrice, highestPrice]);
+      }, [lowestPrice, highestPrice]);
 
     const fetchProducts = () => {
         fetch(`/api/category/${category}/subcategory/${subcategoryid}/products`, { method: 'GET' })
@@ -44,20 +54,28 @@ function SubCategory() {
                 console.log(data);
                 setProducts(data);
                 setFilteredProducts(data);
+                setMinPrice(Math.min(...data.map(product => product.price)))
+                setMaxPrice(Math.max(...data.map(product => product.price)))
+                setLowestPrice(Math.min(...data.map(product => product.price)))
+                setHighestPrice(Math.max(...data.map(product => product.price)))
+
             })
             .catch(error => {
                 console.error('Error fetching instruments:', error);
             });
     };
 
+
+
     function filterProductsByPrice() {
-        let min = minPrice === '' ? 0 : parseFloat(minPrice);
-        let max = maxPrice === '' ? Number.MAX_SAFE_INTEGER : parseFloat(maxPrice);
+        let min = minPrice === '' || minPrice < lowestPrice ? lowestPrice : minPrice;
+        let max = maxPrice === '' || maxPrice > highestPrice ? highestPrice : maxPrice;
         let filteredProducts = products.filter(product => product.price >= min && product.price <= max);
         return filteredProducts
     }
 
-    function filterProductsByBrand(products : product[]){
+
+    function filterProductsByBrand(products : Product[]){
 if(selectedBrands.length === 0){
     return products
 } else {
@@ -79,13 +97,35 @@ if(selectedBrands.length === 0){
         setFilteredProducts(filteredProductsByPrice);
     };
 
+   function setValuesAndMinPriceMaxPriceWithSlider(newValues){
+    const [min, max] = newValues
+    setMinPrice(min)
+    setMaxPrice(max)
+    setValues(newValues)
+    }
+
+    function setMinPriceWithInputField(e){
+setMinPrice(parseFloat(e.target.value))
+setValues([parseFloat(e.target.value),values[1]])
+
+    }
+
+    function setMaxPriceWithInputField(e){
+        setMaxPrice(parseFloat(e.target.value))
+        setValues([values[0],parseFloat(e.target.value)])
+
+    }
+
+   
+
     const handleProductClick = (id: string) => {
         navigate(`/product/${id}`);
     };
 
 
     return (
-        <div>
+        <div className='subcategory'>
+            <div className='filter-section'>
             <div>
                 <label>Filter by Brand:</label>
                 {Array.from(new Set(products.map(product => product.brand))).map(brand => (
@@ -100,17 +140,28 @@ if(selectedBrands.length === 0){
                     </div>
                 ))}
             </div>
-            <div>
+            <div className='box'>
                 <label>
                     Minimum Price:
-                    <input type="number" value={minPrice} onChange={e => setMinPrice(e.target.value)} />
+                    <input type="number" value={minPrice}  onChange={e => setMinPriceWithInputField(e)} />
                 </label>
                 <label>
                     Maximum Price:
-                    <input type="number" value={maxPrice} onChange={e => setMaxPrice(e.target.value)} />
+                    <input type="number" value={maxPrice} onChange={e => setMaxPriceWithInputField(e)} />
                 </label>
+                <div>
+                </div>
+                <h3>Price <span>Range</span></h3>
+                <div>${values[0]}-${values[1]}</div>
+            <Slider className={'slider'}
+            onChange={(newValues) => setValuesAndMinPriceMaxPriceWithSlider(newValues)}
+            value={values}
+                min={lowestPrice} 
+                max={highestPrice}/>
+            </div>
                 <button onClick={handleSearch}>Search</button>
             </div>
+           
             <ul>
                 {filterProductsByBrand(filteredProducts).map((product, index) => (
                     <li key={index} onClick={() => handleProductClick(product.id)}>
