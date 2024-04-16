f5a4b151a5c571a46a3a2cc2a9a721c71407cc99
Balazs Furedi <115081757+Balazs60@users.noreply.github.com>
2024-04-16 12:16:33 +0200

    fix payment options and refactor cart page

--- music-store-frontend/src/Pages/Header.tsx
+++ music-store-frontend/src/Pages/Header.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Dropdown, ButtonGroup, Row, Col } from 'react-bootstrap';
+import { Dropdown, ButtonGroup } from 'react-bootstrap';
 import '../musicStore.css';
 import { Product } from './Products';
 import '../output.css'
@@ -211,7 +211,7 @@ function Header() {
         <div >
           <button className="flex items-center bg-white text-gray-800 rounded-full focus:outline-none" type="button" onClick={handleCartButtonClick}>
             <i className="bi-cart-fill"></i>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6">
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 0 0-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 0 0-16.536-1.84M7.5 14.25 5.106 5.272M6 20.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Zm12.75 0a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z" />
 </svg>
 
